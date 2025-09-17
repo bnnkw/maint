51d13d7bdecdd1ac231dabcf5ac4c5a854c103a4
bnnkw <rsbof.dev@outlook.com>
2025-09-18 01:07:25 +0900

    Implement the request subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,53 @@
 use std::path::Path;
 
+use clap::{Args, Parser, Subcommand};
 use rusqlite::Connection;
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Request(RequestArgs),
+}
+
+#[derive(Args)]
+struct RequestArgs {
+    contract_id: u32,
+    description: Option<String>,
+    request_date: Option<chrono::NaiveDate>,
+}
+
+impl Default for RequestArgs {
+    fn default() -> Self {
+        RequestArgs {
+            contract_id: 0,
+            description: None,
+            request_date: Some(chrono::Utc::now().date_naive()),
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
+}
+
+fn request(conn: &Connection, args: &RequestArgs) -> Result<usize, rusqlite::Error> {
+    conn.execute(
+        "INSERT INTO request (contract_id, description, request_date)
+            VALUES (:contract_id, :description, :request_date)",
+        &[
+            (":contract_id", &args.contract_id.to_string()),
+            (
+                ":description",
+                &"specified -d option or edited with external editor".to_string(),
+            ),
+            (":request_date", &args.request_date.unwrap().to_string()),
+        ],
+    )
 }
 
 fn init<P>(path: Option<P>) -> Result<Connection, rusqlite::Error>
@@ -60,5 +104,13 @@ mod tests {
         assert!(conn.table_exists(Some("main"), "contract").unwrap());
         assert!(conn.table_exists(Some("main"), "request").unwrap());
         assert!(conn.table_exists(Some("main"), "work").unwrap());
+    }
+
+    #[test]
+    fn test_request() {
+        let conn = init::<&str>(None).unwrap();
+        let args = RequestArgs::default();
+        let nrow = request(&conn, &args).unwrap();
+        assert_eq!(1, nrow);
     }
 }
